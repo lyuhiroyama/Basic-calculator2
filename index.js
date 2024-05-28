2999abd8f0c02b1b02594c91b64a1fc6168b0ed9
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2024-05-28 09:02:23 +0900

    Initial changes

---
+++ index.js
@@ -0,0 +1,24 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+
+function add () {
+
+}
+
+function substract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function operate(num1, operator, num2) {
+
+}
